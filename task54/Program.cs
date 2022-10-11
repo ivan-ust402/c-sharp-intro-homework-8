dab494e8e80f3e18546c374ce3d52c1914ea5cdd
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-11 19:13:45 +0700

    Added basic function

---
+++ task54/Program.cs
@@ -0,0 +1,47 @@
+﻿/* 
+Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+В итоге получается вот такой массив:
+7 4 2 1
+9 5 3 2
+8 4 4 2
+*/
+Console.Clear();
+Console.WriteLine("Введите желаемую размерность массива.");
+Console.Write("m = ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] array = new int[m, n];
+FillArray(array);
+PrintArray(array);
+
+
+
+void FillArray(int[,] array)
+{
+    Random randomGen = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = randomGen.Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
