bdf95848f78bf87e472ee7229cd28b3822b53511
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-11 19:43:54 +0700

    Added basic functions

---
+++ task56/Program.cs
@@ -0,0 +1,52 @@
+﻿/* 
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+*/
+Console.Clear();
+Console.WriteLine("Введите количество строк и столбцов массива.");
+Console.Write("m = ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine()!);
+
+int[,] matrix = new int[m, n];
+FillMatrix(matrix);
+PrintMatrix(matrix);
+
+
+
+
+void FillMatrix(int[,] matrix)
+{
+    Random randomGen = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = randomGen.Next(0, 10);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
