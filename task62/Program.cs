3f15b6ffa2296328983e91ff50277a6e3335f544
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-12 23:50:10 +0700

    First test of FillMatrix function failed

--- task62/Program.cs
+++ task62/Program.cs
@@ -9,6 +9,55 @@
 Console.Clear();
 Console.WriteLine("Спиральное заполнение матрицы 4 х 4");
 
+int[] array = new int[]{01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 14, 15, 16};
+int[,] matrix = new int[4,4];
+FillMatrix(matrix, array);
+PrintMatrix(matrix);
+
+void FillMatrix(int[,] matrix, int[] array) {
+    int row = matrix.GetLength(0) - 1;
+    int column = matrix.GetLength(1) - 1;
+    int lengthOfNumbers = matrix.GetLength(0) * matrix.GetLength(1);
+
+    // Сколько раз нужно пройти по кругу, чтобы заполнить матрицу?
+    int count = 0;
+    int iterator = 0;
+    int index = 0;
+    if (matrix.GetLength(0) % 2 == 0) {
+        count = matrix.GetLength(0) / 2;
+    }
+    else {
+        count = matrix.GetLength(0) / 2 + 1;
+    }
+    // Пока не закончится число повторов, выполнять следующие действия
+    while(iterator != count + 1) {
+        // int maxValueOfOneIteration = 
+        for (int i = iterator; i < matrix.GetLength(0) - iterator; i++)
+        {
+            for (int j = iterator; j < matrix.GetLength(1) - iterator; j++)
+            {
+                // Заполняем верхнюю строку
+                if (i == iterator && j >= iterator) {
+                    matrix[i, j] = array[index++]; 
+                }
+                // Заполняем правый столбец
+                else if (i > iterator && j == column - iterator && i < row - iterator) {
+                    matrix[i, j] = array[index++];
+                }
+                // Заполняем нижнюю строку
+                else if (i == row - iterator && j <= column - iterator) {
+                    matrix[i, column - j] = array[index++]; 
+                }
+                // Заполняем левый столбец
+                else if (i > iterator && j == iterator && i < row - iterator) {
+                    matrix[row - i, j] = array[index++]; 
+                }   
+            }
+        }
+        iterator++;
+    }
+}
+
 
 void PrintMatrix(int[,] matrix)
 {
