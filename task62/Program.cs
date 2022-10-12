362ce9fc76e4f3bffaaf4b0c2e3941be29a84926
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-12 19:28:12 +0700

    Added PrintArray function

---
+++ task62/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+Напишите программу, которая заполнит спирально массив 4 на 4.
+Например, на выходе получается вот такой массив:
+01 02 03 04
+12 13 14 05
+11 16 15 06
+10 09 08 07
+*/
+Console.Clear();
+Console.WriteLine("Спиральное заполнение матрицы 4 х 4");
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
