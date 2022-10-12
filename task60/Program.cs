bd53cc63947fb959ec82da97582a2d02b923fc22
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-12 19:10:27 +0700

    Completed task 60

--- task60/Program.cs
+++ task60/Program.cs
@@ -16,9 +16,23 @@ int y = int.Parse(Console.ReadLine()!);
 Console.Write("z = ");
 int z = int.Parse(Console.ReadLine()!);
 
-int[,,] array3D = new int[x, y, z];
+if (x > 4 && y > 4 && z > 4)
+{
+    Console.WriteLine("В связи с ограничением диапазона значений вводимых элементов в матрицу (числа от 10 до 99) матрица не может быть больше 4 х 4 х 4, для работы с бОльшими матрицами необходимо программно расширить диапазон");
+}
+else if (x < 0 && y < 0 && z < 0)
+{
+    Console.WriteLine("Размерность матрицы не может быть отрицательной!");
+}
+else
+{
+    int[,,] array3D = new int[x, y, z];
+    FillArray3D(array3D);
+    PrintArray3D(array3D);
+}
 
-void FillArray3D(int[,,] array) {
+void FillArray3D(int[,,] array)
+{
     Random randomGen = new Random();
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -26,10 +40,63 @@ void FillArray3D(int[,,] array) {
         {
             for (int k = 0; k < array.GetLength(2); k++)
             {
-                array[i, j, k] = randomGen.Next(10, 100);
+                int number = randomGen.Next(10, 100);
+                // Console.WriteLine($"Main {number}");
+                bool notInArray = NotInArray(array, number);
+                if (notInArray)
+                {
+                    // Console.WriteLine($"В if {number}");
+                    array[i, j, k] = number;
+                }
+                else
+                {
+                    while (!notInArray)
+                    {
+                        number = randomGen.Next(10, 100);
+                        notInArray = NotInArray(array, number);
+                    }
+                    // Console.WriteLine($"В else {number}");
+                    array[i, j, k] = number;
+                }
+
             }
         }
     }
 }
 
-void 
+bool NotInArray(int[,,] array, int number)
+{
+    int count = 0;
+    for (int m = 0; m < array.GetLength(0); m++)
+    {
+        for (int n = 0; n < array.GetLength(1); n++)
+        {
+            for (int p = 0; p < array.GetLength(2); p++)
+            {
+                if (number == array[m, n, p])
+                {
+                    count++;
+                }
+            }
+        }
+    }
+    if (count > 0) return false;
+    else return true;
+}
+
+void PrintArray3D(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        // Console.WriteLine($"x = {i}");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i}, {j}, {k}) ");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
