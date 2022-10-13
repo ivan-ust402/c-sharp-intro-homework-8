61779959735a3cd704d291a50edde4507980a7d8
Ivan Ustyantsev <vangold915@gmail.com>
2022-10-13 17:56:41 +0700

    Created FindIndecesOfMinValues function

--- extra-task-1/Program.cs
+++ extra-task-1/Program.cs
@@ -16,9 +16,56 @@ int n = int.Parse(Console.ReadLine()!);
 int[,] matrix = new int[m, n];
 FillMatrix(matrix);
 PrintMatrix(matrix);
+int[] arrayOfIndeces = FindIndecesOfMinValues(matrix);
+PrintArray(arrayOfIndeces);
 
 
 
+int[] FindIndecesOfMinValues(int[,] matrix) {
+    int minValue = matrix[0, 0];
+    int[] arrayOfindeces = new int[2];
+
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (minValue > matrix[i, j]) {
+                minValue = matrix[i, j];
+            } 
+        }
+    }
+
+    int count = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (minValue == matrix[i, j]) {
+                count++;
+            } 
+        }
+    }
+
+    int[] resultArray = new int[count * 2];
+    int index = 0;
+    while(count != 0) {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (minValue == matrix[i, j]) {
+                    resultArray[index] = i;
+                    resultArray[++index] = j;
+                    count--;
+                    index++;
+                } 
+            }
+        }
+    }
+
+    return resultArray;
+}
 
 
 void FillMatrix(int[,] matrix)
@@ -43,4 +90,14 @@ void PrintMatrix(int[,] matrix)
         }
         Console.WriteLine();
     }
+}
+
+void PrintArray(int[] array)
+{       
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]}   ");
+    }
+    Console.WriteLine();
 }
